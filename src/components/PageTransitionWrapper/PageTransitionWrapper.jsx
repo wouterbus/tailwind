e672f87dc4e9b7085fbfbeafbafc678fe87383ae
wouterbus <59446222+wouterbus@users.noreply.github.com>
2025-05-08 18:42:48 -0300

    Page Transitions

---
+++ src/components/PageTransitionWrapper/PageTransitionWrapper.jsx
@@ -0,0 +1,21 @@
+// components/PageTransitionWrapper.jsx
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  exit: { opacity: 0, y: -20, transition: { duration: 0.4 } },
+};
+
+export default function PageTransitionWrapper({ children }) {
+  return (
+    <motion.div
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      {children}
+    </motion.div>
+  );
+}
